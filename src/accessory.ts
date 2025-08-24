d170a04be3f45c2be3c9a09e524d792bc17f919a
Patrick Roza <contact@patrickroza.com>
2025-08-24 17:55:17 +0200

    format

--- src/accessory.ts
+++ src/accessory.ts
@@ -1,77 +1,118 @@
 import {
-    Service,
-    PlatformAccessory,
-    CharacteristicValue,
-    CharacteristicSetCallback,
-    CharacteristicGetCallback,
+  Service,
+  PlatformAccessory,
+  CharacteristicValue,
+  CharacteristicSetCallback,
+  CharacteristicGetCallback,
 } from "homebridge";
 
 import Platform from "./platform";
 
 export default class Accessory {
-    private service: Service;
+  private service: Service;
 
-    private states = {
-        On: false,
-        Brightness: 100,
-    };
+  private states = {
+    On: false,
+    Brightness: 100,
+  };
 
-    constructor(
-        private readonly platform: Platform,
-        private readonly accessory: PlatformAccessory,
-    ) {
-        this.accessory.getService(this.platform.Service.AccessoryInformation)!
-            .setCharacteristic(this.platform.Characteristic.Manufacturer, "Default-Manufacturer")
-            .setCharacteristic(this.platform.Characteristic.Model, "Default-Model")
-            .setCharacteristic(this.platform.Characteristic.SerialNumber, "Default-Serial");
+  constructor(
+    private readonly platform: Platform,
+    private readonly accessory: PlatformAccessory
+  ) {
+    this.accessory
+      .getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(
+        this.platform.Characteristic.Manufacturer,
+        "Default-Manufacturer"
+      )
+      .setCharacteristic(this.platform.Characteristic.Model, "Default-Model")
+      .setCharacteristic(
+        this.platform.Characteristic.SerialNumber,
+        "Default-Serial"
+      );
 
-        this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
-        this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.DisplayName);
+    this.service =
+      this.accessory.getService(this.platform.Service.Lightbulb) ||
+      this.accessory.addService(this.platform.Service.Lightbulb);
+    this.service.setCharacteristic(
+      this.platform.Characteristic.Name,
+      accessory.context.device.DisplayName
+    );
 
-        this.service.getCharacteristic(this.platform.Characteristic.On)
-            .on("set", this.setOn.bind(this))
-            .on("get", this.getOn.bind(this));
+    this.service
+      .getCharacteristic(this.platform.Characteristic.On)
+      .on("set", this.setOn.bind(this))
+      .on("get", this.getOn.bind(this));
 
-        this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-            .on("set", this.setBrightness.bind(this));
+    this.service
+      .getCharacteristic(this.platform.Characteristic.Brightness)
+      .on("set", this.setBrightness.bind(this));
 
-        const motionSensorOneService = this.accessory.getService("Motion Sensor One Name") || this.accessory.addService(this.platform.Service.MotionSensor, "Motion Sensor One Name", "YourUniqueIdentifier-1");
-        const motionSensorTwoService = this.accessory.getService("Motion Sensor Two Name") || this.accessory.addService(this.platform.Service.MotionSensor, "Motion Sensor Two Name", "YourUniqueIdentifier-2");
+    const motionSensorOneService =
+      this.accessory.getService("Motion Sensor One Name") ||
+      this.accessory.addService(
+        this.platform.Service.MotionSensor,
+        "Motion Sensor One Name",
+        "YourUniqueIdentifier-1"
+      );
+    const motionSensorTwoService =
+      this.accessory.getService("Motion Sensor Two Name") ||
+      this.accessory.addService(
+        this.platform.Service.MotionSensor,
+        "Motion Sensor Two Name",
+        "YourUniqueIdentifier-2"
+      );
 
-        let motionDetected = false;
+    let motionDetected = false;
 
-        setInterval(() => {
-            motionDetected = !motionDetected;
+    setInterval(() => {
+      motionDetected = !motionDetected;
 
-            motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-            motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
+      motionSensorOneService.updateCharacteristic(
+        this.platform.Characteristic.MotionDetected,
+        motionDetected
+      );
+      motionSensorTwoService.updateCharacteristic(
+        this.platform.Characteristic.MotionDetected,
+        !motionDetected
+      );
 
-            this.platform.log.debug("Triggering motionSensorOneService:", motionDetected);
-            this.platform.log.debug("Triggering motionSensorTwoService:", !motionDetected);
-        }, 10000);
-    }
+      this.platform.log.debug(
+        "Triggering motionSensorOneService:",
+        motionDetected
+      );
+      this.platform.log.debug(
+        "Triggering motionSensorTwoService:",
+        !motionDetected
+      );
+    }, 10000);
+  }
 
-    setOn(value: CharacteristicValue, callback: CharacteristicSetCallback): void {
-        this.states.On = value as boolean;
+  setOn(value: CharacteristicValue, callback: CharacteristicSetCallback): void {
+    this.states.On = value as boolean;
 
-        this.platform.log.debug("Set Characteristic On ->", value);
+    this.platform.log.debug("Set Characteristic On ->", value);
 
-        callback(null);
-    }
+    callback(null);
+  }
 
-    getOn(callback: CharacteristicGetCallback): void {
-        const isOn = this.states.On;
+  getOn(callback: CharacteristicGetCallback): void {
+    const isOn = this.states.On;
 
-        this.platform.log.debug("Get Characteristic On ->", isOn);
+    this.platform.log.debug("Get Characteristic On ->", isOn);
 
-        callback(null, isOn);
-    }
+    callback(null, isOn);
+  }
 
-    setBrightness(value: CharacteristicValue, callback: CharacteristicSetCallback): void {
-        this.states.Brightness = value as number;
+  setBrightness(
+    value: CharacteristicValue,
+    callback: CharacteristicSetCallback
+  ): void {
+    this.states.Brightness = value as number;
 
-        this.platform.log.debug("Set Characteristic Brightness -> ", value);
+    this.platform.log.debug("Set Characteristic Brightness -> ", value);
 
-        callback(null);
-    }
+    callback(null);
+  }
 }
